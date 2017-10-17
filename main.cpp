0084f936316be1a3c2c85e94c7c1913335de9d8a
scatching <scatching@mail.csuchico.edu>
2017-10-16 21:02:44 -0700

    Project two

--- main.cpp
+++ main.cpp
@@ -20,6 +20,23 @@ using namespace std;
     image.open(file);
     bmp=image.toPixelMatrix();
     cout<<" "<<file<<" has been loaded. It is "<<bmp.size()<<" pixels high and "<<bmp[0].size()<<" wide."<<endl;
+    
+    cout<<"Let's make this image look old."<<endl;
+    int x,y,grey;
+        for(x=0; x<bmp.size();x++)
+        {
+            
+            for(y=0; y<bmp[0].size(); y++)
+            {
+        rgb=bmp[x][y];
+        grey=(rgb.red+rgb.blue+rgb.green) /3;
+        rgb.red=grey;
+        rgb.blue=grey;
+        rgb.green=grey;
+        bmp[x][y]=rgb;
+        }}
+        
+    cout<<"Wow! This belongs in a museum!"<<endl;
 
 //Output to ask user the file name of an image//
 //Find file user inputted and store it in a matrix//
